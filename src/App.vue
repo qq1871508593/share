79684acdee91eda6268d9ec38daa30e69431ec85
mojianpei <jianpei.mo@shengyc.com>
2023-02-02 15:17:00 +0800

    test: 测试git秘钥

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
  * @Author: mojianpei
  * @description: 
  * @Date: 2020-08-01 16:38:45
- * @LastEditors: mojianpei
- * @LastEditTime: 2020-08-16 19:28:32
+ * @LastEditors: 莫某人
+ * @LastEditTime: 2023-02-02 15:16:46
 --> 
 <template>
   <div id="app">
+    胜多负少的
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <router-view></router-view>
